99922d0f3c95d85db0b1e9d581382791671d2c3a
LuckIncome <artas_lider_ordena@mail.ru>
2020-11-16 10:48:52 +0600

    android-studio

---
+++ app/src/main/java/com/example/ecommerce/Sellers/SellerLoginActivity.java
@@ -0,0 +1,75 @@
+package com.example.ecommerce.Sellers;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.ecommerce.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SellerLoginActivity extends AppCompatActivity {
+    private Button registerSellerBegin;
+    private EditText emailInput, passwordInput;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        emailInput = findViewById(R.id.seller_login_email);
+        passwordInput = findViewById(R.id.seller_login_password);
+        registerSellerBegin = findViewById(R.id.seller_login_btn);
+        loadingbar = new ProgressDialog(this);
+
+        registerSellerBegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginSeller();
+            }
+        });
+    }
+
+    private void loginSeller() {
+        final String email = emailInput.getText().toString();
+        final String password = passwordInput.getText().toString();
+
+        if (!email.equals("") && !password.equals("")) {
+            loadingbar.setTitle("Seller Account Login");
+            loadingbar.setMessage("Please wait, while we are checking the credentials.");
+            loadingbar.setCanceledOnTouchOutside(false);
+            loadingbar.show();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(SellerLoginActivity.this, SellerHomeActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                    });
+
+        } else {
+            Toast.makeText(this, "Please complete the Login form.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
